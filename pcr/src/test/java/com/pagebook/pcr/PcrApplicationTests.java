2cb142ca6a29b1f49ade26befb2476fd89420d48
vasaviagarwal <vasavi.agarwal@gmail.com>
2021-01-21 18:48:43 +0530

    Added folders for all services

---
+++ pcr/src/test/java/com/pagebook/pcr/PcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagebook.pcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
